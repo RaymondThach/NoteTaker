087f7a3591430daed72c12019c0189013e7a717d
RaymondThach <raymondnthach@gmail.com>
2025-01-18 02:36:38 +1000

    added Routing in combination with tide authenticator, renamed App.js to Home.js to be a page

--- src/index.js
+++ src/index.js
@@ -1,39 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-//import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home";
 import { createRoot } from "react-dom/client";
 import IAMService from "./IAMService";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 
+//Unprotected route
 const RenderOnAnonymous = ({ children }) => (!IAMService.isLoggedIn() ? children : null);
 
+//Protected route
 const RenderOnAuthenticated = ({ children }) => (IAMService.isLoggedIn() ? children : null);
 
-const IsAllowedToViewProfile = () => (IAMService.hasOneRole("default-roles-myrealm") ? "Yes" : "No");
+//const IsAllowedToViewProfile = () => (IAMService.hasOneRole("default-roles-myrealm") ? "Yes" : "No");
+
+//Redirect to Tide Login form
+const GoToTide = () => IAMService.doLogin();
+
+// const App = () => (
+//   <div>
+//     <RenderOnAnonymous>
+//       <div>
+//         <h1>Hello!</h1>
+//         <p>Please authenticate yourself!</p>
+//         <p><button onClick={() => IAMService.doLogin()}>Login</button></p>
+//       </div>
+//     </RenderOnAnonymous>
+//     <RenderOnAuthenticated>
+//       <div>
+//         <p>Signed in as <b>{IAMService.getName()}</b></p>
+//         <p>Has Default Roles? <b>{IsAllowedToViewProfile()}</b></p>
+//         <p><button onClick={() => IAMService.doLogout()}>Logout</button></p>
+//       </div>
+//     </RenderOnAuthenticated>
+//   </div>
+// );
 
 const App = () => (
-  <div>
+  <BrowserRouter>
     <RenderOnAnonymous>
-      <div>
-        <h1>Hello!</h1>
-        <p>Please authenticate yourself!</p>
-        <p><button onClick={() => IAMService.doLogin()}>Login</button></p>
-      </div>
+      <GoToTide/>
     </RenderOnAnonymous>
     <RenderOnAuthenticated>
-      <div>
-        <p>Signed in as <b>{IAMService.getName()}</b></p>
-        <p>Has Default Roles? <b>{IsAllowedToViewProfile()}</b></p>
-        <p><button onClick={() => IAMService.doLogout()}>Logout</button></p>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </RenderOnAuthenticated>
-  </div>
+  </BrowserRouter>
 );
 
-const renderApp = () => createRoot(document.getElementById("root")).render(<App />);
+const renderApp = () => ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
+//Initiate TideCloak adapter over the notetaker app
 IAMService.initIAM(renderApp);
 
 
---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import "./Home.css";
+import Img_note from "../images/note1.png";
+
+const note_categories = ["Ray", "sasha", "tide", "test", "gasgasgmksgmaskgmkamgka", "more"];
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        NoteTaker
+      </header>
+      {
+        note_categories.length === 0 ? <p>No sticky notes yet.</p> : 
+        <div class = "categories">
+        {
+          note_categories.map((category) => 
+            <div class = "category">
+              <img class = "noteImg" src = {Img_note}/>
+              <p class = "category_name">{category}</p>
+            </div>
+          )
+        }
+        </div>
+      }
+    </div>
+  );
+}
+
+export default Home;
