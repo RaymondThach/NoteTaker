5af563e0cc406605fc50b2ee4aa9c4428eeeca0f
RaymondThach <raymondnthach@gmail.com>
2025-01-17 19:48:31 +1000

    added styling for sticky note categories

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Img_note from './images/note1.png';
+
+const note_categories = ["science", "games", "food", "coding", "anime", "secrets", "Ray", "5kgkasdmgldasgmlasgmlw3imli23gasgasgasgasgasgagsagagsagsgsagasgasgasggasgasgasgasgsgagasgasgasgasasagagsagasgssgasm"];
 
 function App() {
   return (
@@ -6,6 +9,16 @@ function App() {
       <header className="App-header">
         NoteTaker
       </header>
+      <div class = 'categories'>
+        {
+          note_categories.map((category) => 
+            <div class = 'category'>
+              <img class = 'noteImg' src = {Img_note}/>
+              <p class = 'category_name'>{category}</p>
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
