3aecb0aadb42c29b7d7d4cea666bea314185b79a
RaymondThach <raymondnthach@gmail.com>
2025-01-18 00:09:24 +1000

    added tidecloak authentication

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import './App.css';
-import Img_note from './images/note1.png';
+// import './App.css';
+// import Img_note from './images/note1.png';
 
-const note_categories = ["science", "games", "food", "coding", "anime", "secrets", "Ray", "5kgkasdmgldasgmlasgmlw3imli23gasgasgasgasgasgagsagagsagsgsagasgasgasggasgasgasgasgsgagasgasgasgasasagagsagasgssgasm"];
+// const note_categories = [];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        NoteTaker
-      </header>
-      {
-        note_categories.length === 0 ? <p>No sticky notes yet.</p> : 
-        <div class = 'categories'>
-        {
-          note_categories.map((category) => 
-            <div class = 'category'>
-              <img class = 'noteImg' src = {Img_note}/>
-              <p class = 'category_name'>{category}</p>
-            </div>
-          )
-        }
-        </div>
-      }
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         NoteTaker
+//       </header>
+//       {
+//         note_categories.length === 0 ? <p>No sticky notes yet.</p> : 
+//         <div class = 'categories'>
+//         {
+//           note_categories.map((category) => 
+//             <div class = 'category'>
+//               <img class = 'noteImg' src = {Img_note}/>
+//               <p class = 'category_name'>{category}</p>
+//             </div>
+//           )
+//         }
+//         </div>
+//       }
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import { createRoot } from "react-dom/client";
+import IAMService from "./IAMService";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const RenderOnAnonymous = ({ children }) => (!IAMService.isLoggedIn() ? children : null);
+
+const RenderOnAuthenticated = ({ children }) => (IAMService.isLoggedIn() ? children : null);
+
+const IsAllowedToViewProfile = () => (IAMService.hasOneRole("default-roles-myrealm") ? "Yes" : "No");
+
+const App = () => (
+  <div>
+    <RenderOnAnonymous>
+      <div>
+        <h1>Hello!</h1>
+        <p>Please authenticate yourself!</p>
+        <p><button onClick={() => IAMService.doLogin()}>Login</button></p>
+      </div>
+    </RenderOnAnonymous>
+    <RenderOnAuthenticated>
+      <div>
+        <p>Signed in as <b>{IAMService.getName()}</b></p>
+        <p>Has Default Roles? <b>{IsAllowedToViewProfile()}</b></p>
+        <p><button onClick={() => IAMService.doLogout()}>Logout</button></p>
+      </div>
+    </RenderOnAuthenticated>
+  </div>
 );
+
+const renderApp = () => createRoot(document.getElementById("root")).render(<App />);
+
+IAMService.initIAM(renderApp);
+
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
